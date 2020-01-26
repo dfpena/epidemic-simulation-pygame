502b908e21ed92963be95fdc66681649138ff18d
dfpena <user@Plurimac.local>
2020-01-26 06:44:09 -0800

    added a true to the current numbers epidemic simulation of the wuhan virus

--- episim.py
+++ episim.py
@@ -2,11 +2,15 @@
 #!/usr/bin/env python
 from decorator import decorator
 
-import random
-import numpy as np
-import pygame.gfxdraw as gfx
-import pickle
 import pygame as pg
+import time
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+import pygame.gfxdraw as gfx
+import random
+import pickle
 
 
 @decorator
@@ -53,6 +57,13 @@ def start():
     return {'Start':True,'Settings':{'Verbose':True},'Status':{},'Threads':[]}
 
  
+@on_start
+def loadPickle_node_26(*args,**kwargs):
+    kwargs = pickle.load(open("nodegraph6000.pkl","rb"))
+    kwargs['Threads'] = []
+
+    return kwargs
+ 
 def remap(narray,resolution):
     x = np.interp(narray[0],[-1,1],[15,resolution[0]-15])
     y = np.interp(narray[1],[-1,1],[50,resolution[1]- 15])
@@ -60,12 +71,14 @@ def remap(narray,resolution):
 
 
 @on_start
-def simLoop1(*args, **kwargs):
+def simLoop_node_25(*args, **kwargs):
 ############################## EDIT THESE PARAMETERS ########################
-    inflength = 8
-    mortality = 0.0015
-    rnaught = 2
-    infprob = 0
+    inflength = 20
+    mortality = 0.13
+    rnaught = 0
+    infprob = 0.045
+    infincubation=5
+    infdetected = 10
 #############################################################################
 
     if infprob == 0:
@@ -76,22 +89,22 @@ def simLoop1(*args, **kwargs):
     pos = kwargs['Settings']['NetworkX']['Pos']
     G = kwargs['Data']
     screensize = (800,800)
-    G.graph['colors'] = {"Naive":(99, 7, 238),"Infected":(145, 238, 7),"Immune":(255, 235, 59),"Dead":(239, 99, 7)}
-    G.graph['disease'] = {"Infection Probability" : infprob, "Infection Length" : inflength, "Mortality": mortality}
+    G.graph['colors'] = {"Naive":(99, 7, 238),"Infected Symptomatic":(145, 238, 7),"Infected Asymptomatic":(159, 190, 126),"Immune":(255, 235, 59),"Dead":(239, 99, 7)}
+    G.graph['disease'] = {"Infection Probability" : infprob,"Infection Detection":infdetected, "Infection Length" : inflength, "Incubation Length":infincubation, "Mortality": mortality}
     # Add Parameters to Nodegraph
     for nid in G:
-        G.nodes[nid]['Status'] = 'Naive'
-        G.nodes[nid]['Day'] = 0
+        G.node[nid]['Status'] = 'Naive'
+        G.node[nid]['Day'] = 0
     # Remap -1 1 square to  pygame resolution
     for nid in pos:
         pos[nid] = remap(pos[nid],screensize) 
-    G.nodes[0]['Status'] = 'Infected'
+    G.node[0]['Status'] = 'Infected Symptomatic'
     # initialize the pygame module
     pg.init()
     # load and set the logo
     #logo = pg.image.load("logo.png")
     #pg.display.set_icon(logo)
-    pg.display.set_caption("Normal Flu")
+    pg.display.set_caption("Novel coronavirus (2019-nCoV) Wuhan Virus")
     legend = pg.image.load('legend.png')
      
     # create a surface on screen that has the size of 240 x 180
@@ -103,7 +116,13 @@ def simLoop1(*args, **kwargs):
     green = (255, 255, 255) 
     deaths = 0
     infected = 1
+    asymptomatic=1
+    symptomatic=0 
+    willdie=0
     infday = 0 
+    detected = 0
+    immune = 0 
+    datacontainer={"Day":[],"Deaths":[],"Cumulative Infections":[],"Detected Infections":[],"Current Undetected Infections":[]}
     font = pg.font.Font(pg.font.match_font('lato'), 20)
     text = font.render('R0: {0}'.format(rnaught), True, green)
     textinfected = font.render('Infected: {0}'.format(infected), True, green)
@@ -112,42 +131,86 @@ def simLoop1(*args, **kwargs):
      
     # main loop
     while running:
-        infected = 0
-        if infday == 366:
+        #infected = 0
+        if deaths >= 60:
             running = False
+
         screen.blit(background, (0, 0))
-        pg.draw.rect(background,(74,20,140),(0,0,screensize[0],80),0)
-        screen.blit(text, (10,10))
+        pg.draw.rect(background,(74,20,140),(0,0,screensize[0],100),0)
+
         textdeath = font.render('Deaths: {}'.format(deaths), True, green)
-        screen.blit(textdeath, (10,40))
+        screen.blit(textdeath, (10,10))
+        textinfected = font.render('Cumulative Infections: {}'.format(infected), True, green)
+        screen.blit(textinfected, (10,25))
+        
+        textinfected = font.render('Recoveries: {}'.format(immune), True, green)
+        screen.blit(textinfected, (10,55))
+        textinfected = font.render('Naive: {}'.format(kwargs['Settings']['NetworkX']['nodes']-infected), True, green)
+        screen.blit(textinfected, (10,40))
+        totaldetected = infected - (asymptomatic+immune+deaths)
+        textsymptomatic = font.render('Cumulative Detected Infections: {}'.format(totaldetected), True, green)
+        screen.blit(textsymptomatic, (10,70))
+        
+        textasymptomatic = font.render('Current Undetected Infections: {}'.format(asymptomatic), True, green)
+        screen.blit(textasymptomatic, (10,85))
+        
         textdays = font.render('Epidemic Day: {}'.format(infday), True, green)
         screen.blit(textdays, ((screensize[0]/2)-40,10))
+        textrnaught = font.render('R0: {}'.format(rnaught), True, green)
+        screen.blit(textrnaught, ((screensize[0]/2)-40,25))
+        
+        textmortality = font.render('Mortality Rate: {} %'.format(mortality*100), True, green)
+        screen.blit(textmortality, ((screensize[0]/2)+40,25))
+        datacontainer['Day'].append(infday)
+        datacontainer['Deaths'].append(deaths)
+        datacontainer['Cumulative Infections'].append(infected)
+        datacontainer['Detected Infections'].append(totaldetected)
+        datacontainer['Current Undetected Infections'].append(asymptomatic)
+        
         for nid in pos:
             for e in G.edges(nid):
                 cnode= e[1]
                 gfx.line(screen,pos[nid][0],pos[nid][1],pos[cnode][0],pos[cnode][1],(120,144,156))
         for nid in pos:
-            color = G.graph['colors'][G.nodes[nid]['Status']]
+            color = G.graph['colors'][G.node[nid]['Status']]
             gfx.aacircle(screen, pos[nid][0],pos[nid][1], 6, color)
             gfx.filled_circle(screen, pos[nid][0],pos[nid][1], 6, color)
-            if G.nodes[nid]['Status'] == "Infected":
-                infected = infected+1
-                if G.nodes[nid]['Day'] == G.graph['disease']["Infection Length"]:
+            
+            if G.node[nid]['Day'] == G.graph['disease']['Infection Detection']:
+                detected = detected + 1    
+            if G.node[nid]['Status'] == "Infected Symptomatic":
+                if G.node[nid]['Day'] == G.graph['disease']["Infection Length"]:
                     roll = random.random()
                     if roll >= G.graph['disease']['Mortality']:
-                        G.nodes[nid]['Status'] = "Immune"
+                        G.node[nid]['Status'] = "Immune"
+                        immune = immune+1
+                        symptomatic = symptomatic-1
                     if roll < G.graph['disease']['Mortality']:
-                        G.nodes[nid]['Status'] = "Dead"
+                        G.node[nid]['Status'] = "Dead"
+                        symptomatic = symptomatic-1
                         deaths=deaths+1
-                G.nodes[nid]['Day'] = G.nodes[nid]['Day'] + 1
+                G.node[nid]['Day'] = G.node[nid]['Day'] + 1
                 for e in G.edges(nid):
                     cnode = e[1]
-                    if G.nodes[cnode]['Status'] != "Dead": 
-                        if G.nodes[cnode]['Status'] != "Immune":
-                            roll = random.random()
-                            if roll < G.graph['disease']['Infection Probability']:
-                                G.nodes[cnode]['Status'] = 'Infected'
-                
+                    if G.node[cnode]['Status'] != "Dead": 
+                        if G.node[cnode]['Status'] != "Immune":
+                            if G.node[cnode]['Status'] != "Infected Asymptomatic":
+                                if G.node[cnode]['Status'] != "Infected Symptomatic":
+                                    roll = random.random()
+                                    
+                                    if roll < G.graph['disease']['Infection Probability']:
+                                        G.node[cnode]['Status'] = 'Infected Asymptomatic'
+                                        infected = infected+1
+                                        asymptomatic = asymptomatic+1
+                                
+                                
+            if G.node[nid]['Status'] == "Infected Asymptomatic":
+                if G.node[nid]['Day'] >= G.graph['disease']["Incubation Length"]:
+                    asymptomatic = asymptomatic-1
+                    symptomatic = symptomatic+1
+                    G.node[nid]['Status'] = "Infected Symptomatic"
+                G.node[nid]['Day'] = G.node[nid]['Day'] + 1 
+        
     # event handling, gets all event from the event queue
         for event in pg.event.get():
     # only do something if the event is of type QUIT
@@ -155,17 +218,22 @@ def simLoop1(*args, **kwargs):
     # change the value to False, to exit the main loop
                 running = False
         infday = infday + 1
+        
         screen.blit(legend,(625,-15))
         pg.display.flip()
-    return kwargs
- 
- 
-@on_start
-def loadPickle2(*args,**kwargs):
-    kwargs = pickle.load(open("nodegraph.pkl","rb"))
-    kwargs['Threads'] = []
+    time.sleep(10)
+    df=pd.DataFrame(datacontainer)
+    print(df)
+    df = df[['Deaths','Detected Infections','Cumulative Infections']].rolling(1).sum()
+    #sns.barplot(x = 'Day', y = 'Cumulative Infections', data = df)
+    #sns.barplot(x = 'Day', y = 'Detected Infections', palette = 'magma', data = df)
+    sns.lineplot(data=df, linewidth=2)
 
+
+    plt.show()
+    
     return kwargs
+ 
  
 
 
@@ -174,18 +242,18 @@ class StremeNode:
         pass
 
     def run(self,*args,**kwargs):
-        self.kwargs=simLoop1(**loadPickle2(**kwargs))
+        self.kwargs=simLoop_node_25(**loadPickle_node_26(**kwargs))
         return (self.kwargs)
 
 class liveprocess:
     def __init__(self):
-        self.status="pending"
-    def run(self,expname):
-        self.response=simLoop1(**loadPickle2(**start()))
-        self.status="completed"
-        return(self.status)
+        pass
+        
+    def run(self,expname="Local"):
+        self.response=simLoop_node_25(**loadPickle_node_26(**start()))
+        return(self.response)
 
 if __name__ == '__main__':
     process = liveprocess()
-    process.run('Local')
+    process.run()
     
